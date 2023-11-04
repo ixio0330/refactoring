a568613d6b2e5dea5bb667d5277ec5784f092202
ixio0330 <ixio0330@gmail.com>
2023-11-04 11:47:30 +0900

    ♻️ 총액 계산 로직 함수 추출 후 변수 인라인

--- chapter01/index.js
+++ chapter01/index.js
@@ -32,7 +32,6 @@ const invoices = {
 };
 
 function statement(invoices, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoices.customer})\n`;
 
   function usd(aNumber) {
@@ -88,14 +87,21 @@ function statement(invoices, plays) {
     }
     return result;
   }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoices.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
   
   for (let perf of invoices.performances) {
     // 청구 내역 출력
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience})석 \n`;
-    totalAmount += amountFor(perf);
   }
-  
-  result += `총액: ${usd(totalAmount)}\n`;
+
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
