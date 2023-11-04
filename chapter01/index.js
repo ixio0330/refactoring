01a96962a1285272ce996aa7ebcffa4551c97b84
ixio0330 <ixio0330@gmail.com>
2023-11-04 11:34:26 +0900

    ♻️ 공연료 계산 로직 함수로 추출

--- chapter01/index.js
+++ chapter01/index.js
@@ -70,14 +70,19 @@ function statement(invoices, plays) {
     }
     return result;
   }
-  
-  for (let perf of invoices.performances) {
-    // 포인트 적립
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
     volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트 제공
     if ('comedy' === playFor(perf).type) {
       volumeCredits += Math.floor(perf.audience / 5);
     }
+    return volumeCredits;
+  }
+  
+  for (let perf of invoices.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+    
     // 청구 내역 출력
     result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience})석 \n`;
     totalAmount += amountFor(perf);
