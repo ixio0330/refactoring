f4ae8252a79b8ac59cc661e159b7ee8e9174360c
ixio0330 <ixio0330@gmail.com>
2023-11-05 11:57:58 +0900

    ♻️ createStatementData 내부에서 사용하는 함수들 이동

--- chapter01/index.js
+++ chapter01/index.js
@@ -31,49 +31,6 @@ const invoices = {
   ]
 };
 
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case 'tragedy':
-      result = 40_000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30_000;
-      if (aPerformance.audience > 20) {
-        result += 10_000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-  }
-  return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ('comedy' === aPerformance.play.type) {
-    result += Math.floor(aPerformance.audience / 5);
-  }
-  return result;
-}
-
-function totalAmount(data) {
-  return data.performances.reduce((total, p) => total + p.amount, 0);
-}
-
-function totalVolumeCredits(data) {
-  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-}
-
 function usd(aNumber) {
   return new Intl.NumberFormat(
     'en-us', 
@@ -96,15 +53,6 @@ function renderPlainText(data) {
   return result;
 }
 
-// 객체 복사 이유: 가변 데이터는 금방 상하므로 불변처럼 취급하기 위함
-function enrichPerformance(aPerformance) {
-  const result = { ...aPerformance };
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
-  return result;
-}
-
 function createStatementData(invoice) {
   const statementData = {
     customer: invoice.customer,
@@ -113,6 +61,57 @@ function createStatementData(invoice) {
   statementData.totalAmount = totalAmount(statementData);
   statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return statementData;
+
+  function enrichPerformance(aPerformance) {
+    const result = { ...aPerformance };
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+  
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40_000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30_000;
+        if (aPerformance.audience > 20) {
+          result += 10_000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+    }
+    return result;
+  }
+  
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
+  
+  function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+  
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
 }
 
 function statement(invoice) {
