6ab0169a8a44ebdb0137ac9f2c377a38097213ed
ixio0330 <ixio0330@gmail.com>
2023-11-05 22:39:36 +0900

    ♻️ 조건부 로직 다형성으로 변경

--- chapter01/index.js
+++ chapter01/index.js
@@ -38,25 +38,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case 'tragedy':
-        result = 40_000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30_000;
-        if (this.performance.audience > 20) {
-          result += 10_000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.performance.play.type}`)
-    }
-    return result;
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
   }
 
   get volumeCredits() {
@@ -66,6 +48,38 @@ class PerformanceCalculator {
       result += Math.floor(this.performance.audience / 5);
     }
     return result;
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40_000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30_000;
+    if (this.performance.audience > 20) {
+      result += 10_000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch(aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${this.performance.play.type}`)
   }
 }
 
@@ -101,7 +115,7 @@ function createStatementData(invoice) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance)); // 공연료 계산기
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance)); // 팩터리 함수 이용
     const result = { ...aPerformance };
     result.play = calculator.play;
     result.amount = calculator.amount;
