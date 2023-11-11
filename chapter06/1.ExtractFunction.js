a038bc4ef81edd4dce74ec6c9e8de1198c069142
ixio0330 <ixio0330@gmail.com>
2023-11-11 11:40:03 +0900

    ♻️ 지역 변수의 값을 변경할 때

--- chapter06/1.ExtractFunction.js
+++ chapter06/1.ExtractFunction.js
@@ -1,15 +1,9 @@
 // 함수 추출하기
 
 function printOwing(invoice) {
-  let outstanding = 0;
 
   printBanner();
-
-  // 미해결 채무(outstanding) 계산
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
-
+  const outstanding = calculateOutstanding(invoice);
   recordDueDate();
   printDetails(invoice, outstanding);
 }
@@ -29,4 +23,12 @@ function printDetails(invoice, outstanding) {
 function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function calculateOutstanding(invoice) {
+  let result = 0;
+  for (const o of invoice.orders) {
+    result += o.amount;
+  }
+  return result;
 }
