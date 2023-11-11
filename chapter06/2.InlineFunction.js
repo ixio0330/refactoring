fa6d29131f304733de26d1bab37f56d8e084a976
ixio0330 <ixio0330@gmail.com>
2023-11-11 11:46:10 +0900

    ♻️ 단계를 잘게 나눠서 처리하기

--- chapter06/2.InlineFunction.js
+++ chapter06/2.InlineFunction.js
@@ -10,11 +10,7 @@ function moreThanFiveLateDelivers(aDriver) {
 
 function reportLines(aCustomer) {
   const lines = [];
-  gatherCustomerData(lines, aCustomer);
-  return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
   out.push(['name', aCustomer.name]);
   out.push(['location', aCustomer.location]);
+  return lines;
 }
