8dc147dbdaecab090fbb102487f8ea1620e04313
ixio0330 <ixio0330@gmail.com>
2023-11-11 11:37:16 +0900

    ♻️ 유효범위를 벗어나는 변수가 없을 때

--- chapter06/1.ExtractFunction.js
+++ chapter06/1.ExtractFunction.js
@@ -3,9 +3,7 @@
 function printOwing(invoice) {
   let outstanding = 0;
 
-  console.log('****************');
-  console.log('*** 고객 채무 ***');
-  console.log('****************');
+  printBanner();
 
   // 미해결 채무(outstanding) 계산
   for (const o of invoice.orders) {
@@ -16,8 +14,17 @@ function printOwing(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-  // 세부 사항 출력
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
+
+  function printDetails() {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  }
+}
+
+function printBanner() {
+  console.log('****************');
+  console.log('*** 고객 채무 ***');
+  console.log('****************');
 }
