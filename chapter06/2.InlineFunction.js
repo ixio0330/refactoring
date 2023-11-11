3e2d7fd9cd6a1078c852ac4932fb0fd130f56b2e
ixio0330 <ixio0330@gmail.com>
2023-11-11 11:43:17 +0900

    ♻️ 함수 호출문 덮어쓰기

--- chapter06/2.InlineFunction.js
+++ chapter06/2.InlineFunction.js
@@ -1,7 +1,7 @@
 // 함수 인라인하기
 
 function rating(aDriver) {
-  return moreThanFiveLateDelivers(aDriver) ? 2: 1;
+  return aDriver.numberOfLateDelivers > 5 ? 2: 1;
 }
 
 function moreThanFiveLateDelivers(aDriver) {
