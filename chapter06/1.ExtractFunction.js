77c1d36a23f3eeecfc1b60b490072a75d54afefe
ixio0330 <ixio0330@gmail.com>
2023-11-11 11:33:36 +0900

    ✨ 6.1 함수 추출하기

---
+++ chapter06/1.ExtractFunction.js
@@ -0,0 +1,23 @@
+// 함수 추출하기
+
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  console.log('****************');
+  console.log('*** 고객 채무 ***');
+  console.log('****************');
+
+  // 미해결 채무(outstanding) 계산
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  // 마감일(dueDate) 기록
+  const today = new Date();
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+  // 세부 사항 출력
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
