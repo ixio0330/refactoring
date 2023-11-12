ce1b63a46c170b99d51508aa99bc8c65de08f68a
ixio0330 <ixio0330@gmail.com>
2023-11-12 21:15:27 +0900

    ♻️ 참조, 갱신 함수 추가

--- chapter06/6.EncapsulateVariable.js
+++ chapter06/6.EncapsulateVariable.js
@@ -1,3 +1,11 @@
 // 변수 캡슐화하기
 
 let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+
+export function getDefaultOwner() { 
+  return defaultOwner; 
+}
+
+export function setDefaultOwner(arg) {
+  defaultOwner = arg;
+}
