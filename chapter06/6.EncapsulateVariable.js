559525af5ec777a9fb8d63f51b491df34f5d6e25
ixio0330 <ixio0330@gmail.com>
2023-11-12 21:16:02 +0900

    ♻️ 변수명, 함수명 변경

--- chapter06/6.EncapsulateVariable.js
+++ chapter06/6.EncapsulateVariable.js
@@ -1,11 +1,11 @@
 // 변수 캡슐화하기
 
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+let defaultOwnerData = { firstName: '마틴', lastName: '파울러' };
 
-export function getDefaultOwner() { 
-  return defaultOwner; 
+export function defaultOwner() { 
+  return defaultOwnerData; 
 }
 
 export function setDefaultOwner(arg) {
-  defaultOwner = arg;
+  defaultOwnerData = arg;
 }
