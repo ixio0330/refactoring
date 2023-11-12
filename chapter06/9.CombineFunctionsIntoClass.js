f7cbf7457accfe724f0a3093f2b6ffce94cbd5af
ixio0330 <ixio0330@gmail.com>
2023-11-12 23:00:06 +0900

    ✨ 여러 함수를 클래스로 묶기

---
+++ chapter06/9.CombineFunctionsIntoClass.js
@@ -0,0 +1,33 @@
+// 여러 함수를 클래스로 묶기
+
+/**
+ * 함수들이 공유하는 공통 데이터 레코드를 캡슐화
+ * 공통 레코드를 사용하는 함수 각각을 새 클래스로 옮기기
+ * 데이터를 조작하는 로직들은 함수로 추출해서 새 클래스로 옮기기
+ */
+
+const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+
+// client1
+{
+  const aReading = acquireReading();
+  const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+// client2
+{
+  const aReading = acquireReading();
+  const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+  const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+}
+
+// client3
+{
+  const aReading = acquireReading();
+  const basicCharge = calculateBaseCharge(aReading);
+
+  // 요금 계산 함수
+  function calculateBaseCharge(aReading) {
+    return baseRate(aReading.month, aReading.year) * aReading.quantity;
+  }
+}
