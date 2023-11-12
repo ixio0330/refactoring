f546e0fae1942a895c670512d134560d43cd57e2
ixio0330 <ixio0330@gmail.com>
2023-11-12 21:18:42 +0900

    ♻️ 레코드로 캡슐화

--- chapter06/6.EncapsulateVariable.js
+++ chapter06/6.EncapsulateVariable.js
@@ -3,9 +3,18 @@
 let defaultOwnerData = { firstName: '마틴', lastName: '파울러' };
 
 export function defaultOwner() { 
-  return { ...defaultOwnerData }; 
+  return new Person(defaultOwnerData); 
 }
 
 export function setDefaultOwner(arg) {
   defaultOwnerData = arg;
+}
+
+class Person {
+  constructor({ lastName, firstName }) {
+    this._lastName = lastName;
+    this._firstName = firstName;
+  }
+  get lastName() { return this._lastName; }
+  get firstName() { return this._firstName; }
 }
