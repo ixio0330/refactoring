b640b949d94d5b66fb3a8188d8406ac41a24b96d
ixio0330 <ixio0330@gmail.com>
2023-11-12 21:17:10 +0900

    ♻️ 값 캡슐화

--- chapter06/6.EncapsulateVariable.js
+++ chapter06/6.EncapsulateVariable.js
@@ -3,7 +3,7 @@
 let defaultOwnerData = { firstName: '마틴', lastName: '파울러' };
 
 export function defaultOwner() { 
-  return defaultOwnerData; 
+  return { ...defaultOwnerData }; 
 }
 
 export function setDefaultOwner(arg) {
