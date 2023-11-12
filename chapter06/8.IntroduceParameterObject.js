badf0eeb57810bd6307b4ecf15ca5f786d7d5e1b
ixio0330 <ixio0330@gmail.com>
2023-11-12 21:23:06 +0900

    ✨ 매개변수 객체 만들기

---
+++ chapter06/8.IntroduceParameterObject.js
@@ -0,0 +1,24 @@
+// 매개변수 객체 만들기
+
+const station = {
+  name: 'ZB1',
+  readings: [
+    { temp: 47, time: '2023-11-12 20:10'},
+    { temp: 53, time: '2023-11-12 20:20'},
+    { temp: 58, time: '2023-11-12 20:30'},
+    { temp: 53, time: '2023-11-12 20:40'},
+    { temp: 51, time: '2023-11-12 20:50'},
+  ]
+}
+
+// 정상 범위 벗어난 측정값 찾는 함수
+function readingsOutsideRange(station, min, max) {
+  return station.readings.filter(r => r.temp < min || r.temp > max);
+}
+
+// 호출문
+const alerts = readingsOutsideRange(
+  station, 
+  operationPlan.temperatureFloor, // 최저 온도 
+  operationPlan.temperatureCeiling, // 최고 온도
+);
